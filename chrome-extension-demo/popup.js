356ad7af3b607ec9b5b8c4ccbfe3db1e0ae5b03d
weiyao86 <weiyaoisnull@gmail.com>
2019-02-22 14:15:03 +0800

    圆形进度条

---
+++ chrome-extension-demo/popup.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+	var resultsButton = document.getElementById('getResults');
+	resultsButton.onclick = getResults;
+});
+
+function getResults() {
+	chrome.tabs.query({
+			active: true,
+			currentWindow: true
+		},
+		function(tabs) {
+			chrome.tabs.sendMessage(
+				tabs[0].id, {
+					action: 'checkForWord'
+				},
+				function(response) {
+
+					showResults(response && response.results);
+				}
+			);
+		}
+	);
+}
+
+function showResults(results) {
+	var resultsElement = document.getElementById('results');
+	resultsElement.innerText = results ?
+		'This page uses jQuery' :
+		'This page does NOT use jQuery';
+}
